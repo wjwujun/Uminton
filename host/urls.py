3bc1c4831d9738463cedcc05dc066f25419a2623
wj <1477563131@qq.com>
2017-09-21 16:56:37 +0800

    9.21 No.1 commit

--- host/urls.py
+++ host/urls.py
@@ -15,9 +15,25 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from host.views import index
+from host.views import index,excuteScript,sendFile,executeSql,executeTask,newTask,crondTask,account,script,DBaccount,sql,history
 urlpatterns = [
-    url(r'^$', index.as_view()),
+    url(r'^$', index.as_view()),    #主页
+
+    url(r'^excuteScript', excuteScript.as_view()),   #快速脚本执行
+    url(r'^sendFile', sendFile.as_view()),           #快速分发文件
+    url(r'^executeSql', executeSql.as_view()),      #快速执行sql脚本
+    url(r'^executeTask', executeTask.as_view()),     #常用作业执行
+    url(r'^newTask', newTask.as_view()),            #新建作业
+    url(r'^crondTask', crondTask.as_view()),        #定时作业
+
+
+
+    url(r'^account', account.as_view()),            #账户管理
+    url(r'^script', script.as_view()),              #脚本管理
+    url(r'^DBaccount', DBaccount.as_view()),        #DB账户管理
+    url(r'^sql', sql.as_view()),                    #SQl管理
+
+    url(r'^history', history.as_view()),            #任务历史
 
 
 ]
--- host/views.py
+++ host/views.py
@@ -2,15 +2,105 @@ from django.shortcuts import render,HttpResponse
 from django.views import View
 # Create your views here.
 
-
+#host主页
 class index(View):
     def get(self,request):
         return render(request, 'host/index.html')
 
 
 
+""""
+作业管理
+"""
+
+#快速脚本执行
+class excuteScript(View):
+    def get(self,request):
+        return render(request, 'host/task/excuteScript.html',{
+            'active':'task'
+        })
+
+#快速分发文件
+class sendFile(View):
+    def get(self,request):
+        return render(request, 'host/task/sednFile.html',{
+            'active':'task'
+        })
+
+#快速执行sql脚本
+class executeSql(View):
+    def get(self,request):
+        return render(request, 'host/task/executeSql.html',{
+            'active':'task'
+        })
+#常用作业执行
+class executeTask(View):
+    def get(self,request):
+        return render(request, 'host/task/executeTask.html',{
+            'active':'task'
+        })
+#新建作业
+class newTask(View):
+    def get(self,request):
+        return render(request, 'host/task/newTask.html',{
+            'active':'task'
+        })
+#定时作业
+class crondTask(View):
+    def get(self,request):
+        return render(request, 'host/task/crondTask.html',{
+            'active':'task'
+        })
 
 
+
+"""
+业务管理
+
+"""
+
+
+#账户管理
+class account(View):
+    def get(self,request):
+        return render(request, 'host/admin/account.html',{
+            'active':'active'
+        })
+#脚本管理
+class script(View):
+    def get(self,request):
+        return render(request, 'host/admin/script.html',{
+            'active':'active'
+        })
+
+
+#DB账户管理
+class DBaccount(View):
+    def get(self,request):
+        return render(request, 'host/admin/DBaccount.html',{
+            'active':'active'
+        })
+
+
+
+#SQl管理
+class sql(View):
+    def get(self,request):
+        return render(request, 'host/admin/sql.html',{
+            'active':'active'
+        })
+
+
+""""
+任务历史
+"""
+
+#任务历史
+class history(View):
+    def get(self,request):
+        return render(request, 'host/history/history.html',{
+            'active':'history'
+        })
 
 
 
